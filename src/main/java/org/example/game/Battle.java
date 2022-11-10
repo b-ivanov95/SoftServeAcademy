dccc9b38be378535fbb559f99a12e1a58cefae94
Borislav Ivanov <b_ivanov95@abv.bg>
2022-11-10 18:53:17 +0200

    Initial commit

---
+++ src/main/java/org/example/game/Battle.java
@@ -0,0 +1,12 @@
+package org.example.game;
+
+import org.example.characters.Warrior;
+
+public class Battle {
+    public static boolean fight(
+            Warrior warrior1,
+            Warrior warrior2
+    ) {
+        return false;
+    }
+}
